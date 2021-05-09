a7c679e063ec2c211be5eb2a30507eb563e78463
Try Ajitiono <ballinst@gmail.com>
2021-05-10 00:04:24 +0700

    cp
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- frontend/middlewares/mongoose.ts
+++ frontend/middlewares/mongoose.ts
@@ -24,21 +24,20 @@ if (
 const URI = `mongodb+srv://${MONGODB_ADMIN}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER_URL}/${MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 // Source: https://dev.to/raphaelchaula/adding-mongodb-mongoose-to-next-js-apis-3af.
+// With some modificaiton.
 export const connectDB = (handler: NextHandler) => async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-  if (mongoose.connections[0].readyState) {
-    // Use current db connection.
-    return handler(req, res);
+  if (mongoose.connections[0].readyState === 0) {
+    // When the connection doesn't exist, create a new one.
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
   }
 
-  // When the connection doesn't exist, create a new one.
-  await mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  });
   return handler(req, res);
 };
