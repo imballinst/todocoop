6b38145b3f6586160b194d0098476c23f67a27fe
Try Ajitiono <ballinst@gmail.com>
2021-05-23 22:30:34 +0700

    looks good
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- frontend/pages/Room.tsx
+++ frontend/pages/Room.tsx
@@ -1,10 +1,18 @@
-import { ChangeEvent, memo, useEffect, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  memo,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
 import { Checkbox } from '@chakra-ui/checkbox';
 import { FormHelperText, FormControl } from '@chakra-ui/form-control';
 import { Input } from '@chakra-ui/input';
 import { Box, Heading, HStack, List, ListItem } from '@chakra-ui/layout';
 import { CheckIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons';
 import { Button, IconButton } from '@chakra-ui/button';
+import { Table, TableCellProps, Tbody, Td, Tr } from '@chakra-ui/table';
 import { Controller, useForm, useWatch } from 'react-hook-form';
 
 import { generateHash } from '../lib/utils';
@@ -35,13 +43,16 @@ export function RoomDetail({ room }: RoomProps) {
           {name}
         </Heading>
         <Box p={4}>
-          <List spacing={2} listStyleType="none">
-            {currentTodos.map((todo, index) => (
-              <ListItem>
-                <TodoForm roomName={name} index={index} todo={todo} />
-              </ListItem>
-            ))}
-          </List>
+          <Table variant="simple" width="unset">
+            <Tbody>
+              {currentTodos.map((todo, index) => (
+                <Tr>
+                  <TodoForm roomName={name} index={index} todo={todo} />
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+
           <Button
             mt={4}
             colorScheme="teal"
@@ -137,87 +148,96 @@ const TodoForm = memo(
       deleteTodoMutation.mutate({ name: roomName, todoId: todo._id });
     }
 
-    return (
-      <HStack spacing={2}>
-        {isEditing ? (
-          <>
+    return isEditing ? (
+      <>
+        <TableColumn>
+          <Controller
+            render={({ field }) => (
+              <Checkbox
+                name={field.name}
+                onBlur={field.onBlur}
+                onChange={field.onChange}
+                checked={field.value}
+                ref={field.ref}
+              />
+            )}
+            name="is_checked"
+            control={control}
+          />
+        </TableColumn>
+
+        <TableColumn>
+          <FormControl>
             <Controller
-              render={({ field }) => (
-                <Checkbox
-                  name={field.name}
-                  onBlur={field.onBlur}
-                  onChange={field.onChange}
-                  checked={field.value}
-                  ref={field.ref}
-                />
-              )}
-              name="is_checked"
+              render={({ field }) => <Input {...field} />}
+              name="title"
               control={control}
             />
 
-            <FormControl>
-              <Controller
-                render={({ field }) => <Input {...field} />}
-                name="title"
-                control={control}
-              />
+            {errors[index]?.title && (
+              <FormHelperText>{errors[index]?.title}</FormHelperText>
+            )}
+          </FormControl>
+        </TableColumn>
 
-              {errors[index]?.title && (
-                <FormHelperText>{errors[index]?.title}</FormHelperText>
-              )}
-            </FormControl>
-
+        <TableColumn>
+          <IconButton
+            minWidth="var(--chakra-sizes-6)"
+            height="var(--chakra-sizes-6)"
+            colorScheme="teal"
+            onClick={onSave}
+            aria-label="Save"
+            icon={<CheckIcon />}
+          />
+        </TableColumn>
+      </>
+    ) : (
+      <>
+        <TableColumn colSpan={2}>
+          <Controller
+            render={({ field }) => (
+              <Checkbox
+                name={field.name}
+                onBlur={field.onBlur}
+                checked={field.value}
+                ref={field.ref}
+                onChange={onChangeTick}
+              >
+                {todo.title}
+              </Checkbox>
+            )}
+            name="is_checked"
+            control={control}
+          />
+        </TableColumn>
+        <TableColumn>
+          <HStack spacing={2}>
             <IconButton
               minWidth="var(--chakra-sizes-6)"
               height="var(--chakra-sizes-6)"
               colorScheme="teal"
-              onClick={onSave}
-              aria-label="Save"
-              icon={<CheckIcon />}
+              onClick={() => setIsEditing(true)}
+              aria-label="Edit"
+              icon={<EditIcon />}
             />
-          </>
-        ) : (
-          <>
-            <Controller
-              render={({ field }) => (
-                <Checkbox
-                  name={field.name}
-                  onBlur={field.onBlur}
-                  checked={field.value}
-                  ref={field.ref}
-                  onChange={onChangeTick}
-                >
-                  {todo.title}
-                </Checkbox>
-              )}
-              name="is_checked"
-              control={control}
+            <IconButton
+              minWidth="var(--chakra-sizes-6)"
+              height="var(--chakra-sizes-6)"
+              colorScheme="teal"
+              onClick={onDelete}
+              aria-label="Edit"
+              icon={<DeleteIcon />}
             />
-
-            <HStack spacing={2}>
-              <IconButton
-                minWidth="var(--chakra-sizes-6)"
-                height="var(--chakra-sizes-6)"
-                colorScheme="teal"
-                onClick={() => setIsEditing(true)}
-                aria-label="Edit"
-                icon={<EditIcon />}
-              />
-              <IconButton
-                minWidth="var(--chakra-sizes-6)"
-                height="var(--chakra-sizes-6)"
-                colorScheme="teal"
-                onClick={onDelete}
-                aria-label="Edit"
-                icon={<DeleteIcon />}
-              />
-            </HStack>
-          </>
-        )}
-      </HStack>
+          </HStack>
+        </TableColumn>
+      </>
     );
   }
 );
+
+function TableColumn(props: { children: ReactNode } & TableCellProps) {
+  return <Td {...props} paddingInline={3} py={2} />;
+}
 
 function resolveExistingTodos(todos: BaseTodo[]): BaseTodo[] {
   return todos.map((todo) => ({
