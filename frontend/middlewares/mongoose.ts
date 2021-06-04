92069c7da33d631149bd290122be2618b55f6ebe
Try Ajitiono <ballinst@gmail.com>
2021-06-04 21:29:12 +0700

    ok
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- frontend/middlewares/mongoose.ts
+++ frontend/middlewares/mongoose.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse } from 'next';
 import mongoose from 'mongoose';
 
-import { NextHandler } from '../types';
+import { ExtendedNextApiRequest, NextHandler } from '../types';
 
 const {
   MONGODB_ADMIN,
@@ -25,19 +25,18 @@ const URI = `mongodb+srv://${MONGODB_ADMIN}:${MONGODB_PASSWORD}@${MONGODB_CLUSTE
 
 // Source: https://dev.to/raphaelchaula/adding-mongodb-mongoose-to-next-js-apis-3af.
 // With some modificaiton.
-export const withDB = (handler: NextHandler) => async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  if (mongoose.connections[0].readyState === 0) {
-    // When the connection doesn't exist, create a new one.
-    await mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-  }
+export const withDB =
+  (handler: NextHandler) =>
+  async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
+    if (mongoose.connections[0].readyState === 0) {
+      // When the connection doesn't exist, create a new one.
+      await mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      });
+    }
 
-  return handler(req, res);
-};
+    return handler(req, res);
+  };
