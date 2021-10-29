520991d8683324740d7e6150c9fb34b6d47e37c2
Try Ajitiono <ballinst@gmail.com>
2021-10-29 16:44:02 +0700

    push
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

---
+++ components/HowToUse/HowToUse.tsx
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import path from 'path'
+import { Layout } from '../Layout'
+
+// Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
+const components = {
+  // It also works with dynamically-imported components, which is especially
+  // useful for conditionally loading components for certain routes.
+  // See the notes in README.md for more details.
+  // TestComponent: dynamic(() => import('../../components/TestComponent')),
+  Head,
+}
+
+export function HowToUse({ source, frontMatter }) {
+  return (
+    <Layout isLoggedInToARoom={false} isFetching={false} title="How to Use">
+      <div className="post-header">
+        <h1>{frontMatter.title}</h1>
+        {frontMatter.description && (
+          <p className="description">{frontMatter.description}</p>
+        )}
+      </div>
+      <main>
+        <MDXRemote {...source} components={components} />
+      </main>
+
+      <style jsx>{`
+        .post-header h1 {
+          margin-bottom: 0;
+        }
+
+        .post-header {
+          margin-bottom: 2rem;
+        }
+        .description {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
+  )
+}
